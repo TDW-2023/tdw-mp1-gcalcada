e0d7cafe5563c56189661ce982e2ad126890c8f3
unknown <goncalocalcada7@gmail.com>
2023-10-27 15:49:51 +0100

    Adding settings to deploy on netlify

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true,
   },
   assetPrefix: '/',
-};
+  target: 'server',
+}
+
+
