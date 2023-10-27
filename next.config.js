6ca80d52447f10463a0987169c1def64c8586c73
unknown <goncalocalcada7@gmail.com>
2023-10-27 18:19:40 +0100

    Adding settings to deploy on netlify fix stuff

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
+  trailingSlash: true,
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  output: "export",
-};
+  target: 'server', 
+}
 
-module.exports = nextConfig;
-
+module.exports = nextConfig
